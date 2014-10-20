c7db54cee3f5974dd2e8d29addd9884498ff1d92
Newbsylberry <chris.s.mccarthy@gmail.com>
2014-10-19 22:45:12 -0400

    Add distance

--- front_end/app/scripts/controllers/volunteerhome.js
+++ front_end/app/scripts/controllers/volunteerhome.js
@@ -8,11 +8,44 @@
  * Controller of the voluntrApp
  */
 angular.module('voluntrApp')
-  .controller('VolunteerHomeCtrl', function ($scope, geolocation, Event, Organization) {
-        $scope.user_location = geolocation.getLocation().then(function(data){
-            return {lat:data.coords.latitude, long:data.coords.longitude};
+  .controller('VolunteerHomeCtrl', function ($scope, geolocation, Event,
+                                             Organization, $http, $timeout) {
+        geolocation.getLocation().then(function(data){
+            $scope.user_location = {lat:data.coords.latitude,long:data.coords.longitude};
+
+            console.log($scope.user_location)
         });
 
         $scope.events = Event.all();
 
+        $scope.$on('mapInitialized', function(event, map) {
+
+
+
+        });
+
+        $scope.getDistance = function(event) {
+            $timeout(
+            function() {
+                var origin = '235 Harrison St. Syracuse, NY'
+                var destination = new google.maps.LatLng(event.latitude, event.longitude);
+                var distanceMatrix = new google.maps.DistanceMatrixService();
+                distanceMatrix.getDistanceMatrix(
+                     {
+                         origins: [(origin)],
+                         destinations: [(destination)],
+                         travelMode: google.maps.TravelMode.DRIVING,
+                         unitSystem: google.maps.UnitSystem.IMPERIAL
+                     }, callback);
+                function callback(response, status) {
+                    event.distance = response.rows[0].elements[0].distance.text;
+                    console.log(event)
+                    return $scope.event = event;
+                }
+            }, 250);
+        };
+
+
+
   });
+
