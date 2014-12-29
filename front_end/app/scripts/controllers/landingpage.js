4b512ba7bec24b25faac18811daa21b1dc4691bb
Newbsylberry <chris.s.mccarthy@gmail.com>
2014-12-29 10:17:03 -0500

    Added images for landing page

--- front_end/app/scripts/controllers/landingpage.js
+++ front_end/app/scripts/controllers/landingpage.js
@@ -8,9 +8,9 @@
  * Controller of the voluntrApp
  */
 angular.module('voluntrApp')
-    .controller('LandingPageCtrl', function ($scope, Facebook, Organization,
-                                             $http, $state, Auth, $rootScope, $location,
-                                             $window, $anchorScroll, $document) {
+  .controller('LandingPageCtrl', function ($scope, Facebook, Organization,
+                                           $http, $state, Auth, $rootScope, $location,
+                                           $window, $anchorScroll, $document) {
 
 
     // Set the height and width
@@ -28,13 +28,13 @@ angular.module('voluntrApp')
     var duration = 1000; //milliseconds
 
     //Scroll to the exact position
-   $scope.goToSection = function (section) {
-     var offset = 225; //pixels; adjust for floating menu, context etc
-     //Scroll to #some-id with 30 px "padding"
-     //Note: Use this in a directive, not with document.getElementById
-     var someElement = angular.element(document.getElementById(section));
-     $document.scrollToElement(someElement, offset, duration);
-   }
+    $scope.goToSection = function (section) {
+      var offset = 225; //pixels; adjust for floating menu, context etc
+      //Scroll to #some-id with 30 px "padding"
+      //Note: Use this in a directive, not with document.getElementById
+      var someElement = angular.element(document.getElementById(section));
+      $document.scrollToElement(someElement, offset, duration);
+    }
 
 
     $scope.organizationslide = 'organization-1';
@@ -50,31 +50,37 @@ angular.module('voluntrApp')
       $scope.contactslide = contactsection;
 
     }
-        $scope.organizationLogIn = function () {
-            Facebook.getLoginStatus(function(response) {
-                if(response.status === 'connected') {
-                    $scope.facebook_token = response.authResponse.accessToken;
-                    $state.go('landing_page.organization_landing')
-                }
-                else if (response.status !== 'connected') {
-                    Facebook.login(function(response) {
-                            Facebook.api('/me/accounts', function(response) {
-                                $scope.facebook_token = response.authResponse.accessToken;
-                            });
-                            // Do something with response.
-                        }, {scope: ['manage_pages', 'user_groups']}
-                    );
-                    $state.go('landing_page.organization_landing')
-                };
-            });
-            console.log($scope.organizations)
-
+    $scope.organizationLogIn = function () {
+      Facebook.getLoginStatus(function(response) {
+        if(response.status === 'connected') {
+          $scope.facebook_token = response.authResponse.accessToken;
+          $state.go('landing_page.organization_landing')
+        }
+        else if (response.status !== 'connected') {
+          Facebook.login(function(response) {
+              Facebook.api('/me/accounts', function(response) {
+                $scope.facebook_token = response.authResponse.accessToken;
+              });
+              // Do something with response.
+            }, {scope: ['manage_pages', 'user_groups']}
+          );
+          $state.go('landing_page.organization_landing')
         };
+      });
+      console.log($scope.organizations)
 
+    };
 
+    var ttg = new google.maps.LatLng('43.044622', '-76.150299');
 
-
-
-
-
+    $scope.$on('mapInitialized', function(event, map) {
+      map.setCenter(ttg)
     });
+
+
+
+
+
+
+
+  });
