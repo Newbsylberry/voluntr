3c083550131d581c53f6ede4009ac114299210b0
Newbsylberry <chris.s.mccarthy@gmail.com>
2015-03-28 09:56:18 -0400

    Fixed wrong paths in puma config

--- config/puma.rb
+++ config/puma.rb
@@ -4,12 +4,12 @@ directory '/home/voluser/apps/volu/current'
 rackup '/home/voluser/apps/volu/current/config.ru'
 environment 'production'
 
-pidfile '/home/volu/apps/voluser/shared/tmp/pids/puma.pid'
-state_path '/home/volu/apps/voluser/shared/tmp/pids/puma.state'
-stdout_redirect '/home/volu/apps/voluser/current/log/puma.error.log',
-                '/home/volu/apps/voluser/current/log/puma.access.log', true
+pidfile '/home/voluser/apps/volu/shared/tmp/pids/puma.pid'
+state_path '/home/voluser/apps/volu/shared/tmp/pids/puma.state'
+stdout_redirect '/home/voluser/apps/volu/current/log/puma.error.log',
+                '/home/voluser/apps/volu/current/log/puma.access.log', true
 
 
 threads 0,16
 
-bind 'unix:///home/volu/apps/voluser/shared/tmp/sockets/volu-puma.sock'
+bind 'unix:///home/volu/apps/volu/shared/tmp/sockets/volu-puma.sock'
