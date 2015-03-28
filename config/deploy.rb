d6792b1b7e889cc0bd0f5e17de079a23804a1e39
Newbsylberry <chris.s.mccarthy@gmail.com>
2015-03-27 23:55:58 -0400

    Changed production deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :repo_url, 'git@github.com:Newbsylberry/voluntr.git'
 set :user, 'voluser'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
-set :shared_path, "/home/#{fetch(:user)}/apps/shared"
+set :release_path, "/home/#{fetch(:user)}/apps/current"
 
 set :rvm1_ruby_version, 'rbx'
 
@@ -16,15 +16,6 @@ set :use_sudo,        false
 set :deploy_via,      :remote_cache
 
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-set :puma_preload_app, false
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to true if using ActiveRecord
 
 
 
