504e44b3d5eae6f9e28a418af1c9561edd043304
Newbsylberry <chris.s.mccarthy@gmail.com>
2015-04-02 22:36:28 -0400

    Alternative config

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,5 +1,7 @@
 if Rails.env.production?
-  Resque.redis = "45.55.137.193:6379" # default localhost:6379
+  Resque.configure do |config|
+    config.redis = "45.55.137.193:6379" # default localhost:6379
+  end
 end
 
 # Resque::Plugins::Status::Hash.expire_in = (24 * 60 * 60) # 24hrs in seconds
