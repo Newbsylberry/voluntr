52f4d57962f34c3a44c2364381c726f21d9a1ad3
Newbsylberry <chris.s.mccarthy@gmail.com>
2015-04-02 21:47:27 -0400

    Added redis server

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,5 @@
-# Resque.redis = "0.0.0.0:5678" # default localhost:6379
+if Rails.env.production?
+  Resque.redis = "45.55.137.193:6379" # default localhost:6379
+end
+
 # Resque::Plugins::Status::Hash.expire_in = (24 * 60 * 60) # 24hrs in seconds
