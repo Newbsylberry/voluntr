d83952dd2035cadfc0c12ba4878890f3bc538060
Newbsylberry <chris.s.mccarthy@gmail.com>
2015-05-17 23:57:57 -0400

    Updated whenever time

--- config/schedule.rb
+++ config/schedule.rb
@@ -21,6 +21,6 @@
 job_type :runner, "cd :path && $HOME/.rvm/scripts/rvm && rvm use rbx && bundle exec rails runner ':task' :output"
 
 
-every 1.minutes do
+every 24.hours do
   runner "OrganizationRecordHourJob.set.perform_later"
 end
