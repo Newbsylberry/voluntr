6c4de83b14e96574b47523d789ac60acffa0d5ab
Bing Bai <bert_b@live.com>
2015-05-31 12:34:41 -0400

    sign in

--- front_end/app/organizations/directives/schedule_form_directive.js
+++ front_end/app/organizations/directives/schedule_form_directive.js
@@ -24,7 +24,7 @@ angular.module('voluntrApp').directive("scheduleForm", function () {
         } else {
           $scope.duration_label = 'hours'
         };
-        $scope.calendar.end_time = $scope.calendar.start_time.getTime() + $scope.calendar.duration;
+        //$scope.calendar.end_time = $scope.calendar.start_time.getTime() + $scope.calendar.duration;
       });
 
 
