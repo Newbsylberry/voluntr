3792b66ab21b3d79b7269eeb7e4d7403d72fb07c
Bing Bai <bert_b@live.com>
2015-07-01 10:35:57 -0400

    recorded hours list style

--- front_end/app/navigation/header.js
+++ front_end/app/navigation/header.js
@@ -2,10 +2,25 @@
  * Created by chrismccarthy on 3/9/15.
  */
 angular.module('voluntrApp')
-  .controller('HeaderCtrl', function ($scope, $rootScope, searchService) {
+  .controller('HeaderCtrl', function ($scope, $rootScope, searchService,$window) {
 
     $scope.search_filter = searchService.search;
 
 
+    $(window).scroll(function() {
+        console.log("hello world");
+var distanceY = $window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 300,
+            header = document.querySelector(".application-header");
+        if (distanceY > shrinkOn) {
+            classie.add(header,"smaller");
+        } else {
+            if (classie.has(header,"smaller")) {
+                classie.remove(header,"smaller");
+            }
+        }
+
+});
+    
 
   });
