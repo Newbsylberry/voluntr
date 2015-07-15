43157c4954f1cae256e7238df1c2c1f5f3518ea7
Newbsylberry <chrismccarthy@Chriss-MacBook-Air.local>
2015-07-15 03:40:21 -0600

    Added new side navigation menu, fixing FAB, wait to pull

--- front_end/app/navigation/side-menu-controller.js
+++ front_end/app/navigation/side-menu-controller.js
@@ -4,27 +4,9 @@
 
 angular.module('voluntrApp')
   .controller('SideMenuCtrl', function ($scope, $stateParams, $state, Opportunity,
-                                        $modal, $rootScope) {
+                                        $modal, $rootScope, $mdSidenav) {
 
-    $scope.toggleLeft = buildToggler('left');
 
-    $scope.close = function () {
-      $mdSidenav('left').close()
-        .then(function () {
-          $log.debug("close LEFT is done");
-        });
-    };
-
-    function buildToggler(navID) {
-      var debounceFn =  $mdUtil.debounce(function(){
-        $mdSidenav(navID)
-          .toggle()
-          .then(function () {
-            $log.debug("toggle " + navID + " is done");
-          });
-      },300);
-      return debounceFn;
-    }
 
 
 
