33d0fccafda2b9a2f43e16dbb212302413666f95
Chris McCarthy <chrismccarthy@Chriss-MacBook-Pro.local>
2015-08-24 17:34:00 -0400

    Fixed role bug

--- front_end/app/organizations/modals/add_person_opportunity.js
+++ front_end/app/organizations/modals/add_person_opportunity.js
@@ -22,7 +22,9 @@ angular.module('voluntrApp')
     $scope.addOpportunityPerson = function() {
       var attr = {};
       attr.person_id = person.id;
-      attr.opportunity_role_id = $scope.opportunity_role.id;
+      if ($scope.opportunity_role.id) {
+        attr.opportunity_role_id = $scope.opportunity_role.id;
+      };
       attr.opportunity_id = opportunity.id;
       PersonOpportunity.create(attr)
       $modalInstance.close()
