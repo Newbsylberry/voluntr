f96a54a0f5d46b4addd497d3adf272d78d075566
Chris McCarthy <chrismccarthy@Chriss-MacBook-Pro.local>
2016-01-12 16:35:39 -0500

    Volunteer drive page draft numero dos

--- front_end/app/static_pages/volunteer_drive_16.js
+++ front_end/app/static_pages/volunteer_drive_16.js
@@ -17,31 +17,29 @@ angular.module('voluntrApp')
     $timeout(function(){
        $scope.introduction = false;
        $scope.loaded = true;
-    }, 2500)
+    }, 3000)
 
-    if ($state.is('volunteer_drive_16.form')){
-      $scope.no_buttons = true;
-    }
+
 
     $scope.formScreen = function() {
       $state.go('volunteer_drive_16.form')
       $scope.no_buttons = true;
     }
 
+    $scope.button_text = "How Does It Work?";
+
     $scope.changeState = function(screen) {
       if ($state.is('volunteer_drive_16.1')){
-        $state.go('volunteer_drive_16.2')
+        $state.go('volunteer_drive_16.2');
+        $scope.button_text = 'What Do I Get Out Of It?';
       } if ($state.is('volunteer_drive_16.2')){
+        $scope.button_text = "Alright, I'm Sold Where Do I Sign Up?"
         $state.go('volunteer_drive_16.3')
       } if ($state.is('volunteer_drive_16.3')){
-        $state.go('volunteer_drive_16.4')
-      } if ($state.is('volunteer_drive_16.3')){
         $state.go('volunteer_drive_16.form')
+        $scope.no_buttons = true;
       }
-    }
-
-
-
+    };
 
   });
 
