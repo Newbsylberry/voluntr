183f178ae09fd84fbb3d6905205ca04687cb77a9
Chris McCarthy <chrismccarthy@Chriss-MacBook-Pro.local>
2016-01-24 21:09:24 -0500

    Updated image links for volunteer page of volunteer drive

--- front_end/app/static_pages/volunteer_drive_16/volunteer_drive_16.js
+++ front_end/app/static_pages/volunteer_drive_16/volunteer_drive_16.js
@@ -26,13 +26,13 @@ angular.module('voluntrApp')
 
     $scope.prod = {imagePaths: []};
     $scope.prod.imagePaths = [
-      { custom: 'images/voldrive/logos/frat_council.png', thumbnail: 'images/voldrive/logos/frat_council.png' },
-      { custom: 'images/voldrive/logos/young_leaders_united.jpg', thumbnail: 'images/voldrive/logos/young_leaders_united.jpg' },
-      { custom: 'images/voldrive/logos/centerstate.png', thumbnail: 'images/voldrive/logos/centerstate.png'},
-      { custom: 'images/voldrive/logos/believe_in_cuse.jpg', thumbnail: 'images/voldrive/logos/believe_in_cuse.jpg' },
-      { custom: 'images/voldrive/logos/forty_below.png', thumbnail: 'images/voldrive/logos/forty_below.png' },
-      { custom: 'images/voldrive/logos/onondaga_community_college.jpg', thumbnail: 'images/voldrive/logos/onondaga_community_college.jpg' },
-      { custom: 'images/voldrive/logos/rha.jpg', thumbnail: 'images/voldrive/logos/rha.jpg' }
+      { custom: 'images/frat_council.png', thumbnail: 'images/frat_council.png' },
+      { custom: 'images/young_leaders_united.jpg', thumbnail: 'images/young_leaders_united.jpg' },
+      { custom: 'images/centerstate.png', thumbnail: 'images/centerstate.png'},
+      { custom: 'images/believe_in_cuse.jpg', thumbnail: 'images/believe_in_cuse.jpg' },
+      { custom: 'images/forty_below.png', thumbnail: 'images/forty_below.png' },
+      { custom: 'images/onondaga_community_college.jpg', thumbnail: 'images/onondaga_community_college.jpg' },
+      { custom: 'images/rha.jpg', thumbnail: 'images/rha.jpg' }
     ];
 
     $scope.introduction = true;
