364f7fd82e9edb1d546415a295d4e8a9a4763422
Chris McCarthy <chrismccarthy@Chriss-MacBook-Pro.local>
2016-02-08 09:19:16 -0500

    Updated resque with new ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -14,7 +14,7 @@ server '104.236.68.119', user: 'voluser', roles: %w{web}
 server '45.55.62.200', user: 'voluser', roles: %w{db}
 
 
-role :resque_worker, '162.243.32.12', user: 'voluser'
+role :resque_worker, '162.243.102.244', user: 'voluser'
 set :workers, {"high_priority" => 4, "low_priority" => 2}
 
 # set :whenever_roles, [:resque_worker]
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -11,7 +11,7 @@ server '104.236.113.30', user: 'voluser', roles: %w{app}
 server '104.236.113.30', user: 'voluser', roles: %w{web}
 server '45.55.62.200', user: 'voluser', roles: %w{db}
 
-role :resque_worker, '162.243.32.12', user: 'voluser'
+role :resque_worker, '162.243.102.244', user: 'voluser'
 set :workers, {"high_priority" => 4, "low_priority" => 2}
 
 # set :whenever_roles, [:resque_worker]
