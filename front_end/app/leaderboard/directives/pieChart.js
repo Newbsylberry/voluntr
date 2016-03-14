7f77a98c8c248098f8a8515febf7dede3a044cac
Peter B Smith <peterbutlersmith@gmail.com>
2016-03-14 16:03:54 -0400

    scaffolding pie chart

---
+++ front_end/app/leaderboard/directives/pieChart.js
@@ -0,0 +1,70 @@
+'use strict';
+
+angular.module('voluntrApp')
+
+.directive('pieChart', function() {
+  function link(scope,element,attrs) {
+    //Conventional D3 Margin
+    var margin = {top: 5, right: 5, bottom: 5, left: 5};
+    var width = 100 - margin.left - margin.right;
+    var height = 100 - margin.top - margin.bottom;
+
+    //Boilerplate chart append
+    var chart = d3.select(element[0]).append('svg')
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+              .append("g")
+                .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    var radius = Math.min(width,height) / 2;
+
+    var arc = d3.svg.arc()
+      .innerRadius(0)
+      .outerRadius(radius * 0.9);
+
+    var pie = d3.layout.pie()
+      .sort(null)
+      .value(function(d) { return d.rate; });
+
+    var color = d3.scale.ordinal()
+      .range(["#0000FF", "#FF0000"]);
+
+
+    scope.$watch('data', function(data) {
+      if (!data) return;
+
+      data = [
+        data,
+        {
+          rate: 100 - data.rate
+        }
+      ];
+
+      var arcs = chart.selectAll('.arc')
+                   .data(pie(data))
+                 .enter().append("g")
+                   .attr("class", "arc");
+
+      arcs.append("path")
+       .attr("d", arc)
+       .style("fill", function(d,i) { return color(i); });;
+
+
+     chart.append("text")
+          .attr("x", 0)
+          .attr("y", 0)
+          .style("text-anchor", "center")
+          .style("fill", "#FFFFFF")
+          .text(data[0].rate + "%");
+
+
+    });
+
+  }
+
+  return {
+    restrict: 'E',
+    scope: {data: '='},
+    link: link
+  };
+});
