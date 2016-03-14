6818c6b42fd6b35a147ad6ecb414fc0f29fc1c7d
Peter B Smith <peterbutlersmith@gmail.com>
2016-03-14 16:03:38 -0400

    adding labels to horizontal bar chart

--- front_end/app/leaderboard/directives/horizontalBarChart.js
+++ front_end/app/leaderboard/directives/horizontalBarChart.js
@@ -20,7 +20,6 @@ angular.module('voluntrApp')
 
     scope.$watch('data', function(data) {
       if (!data) return;
-      console.log(data);
 
       var x = d3.scale.linear()
                 .domain([0,d3.max(data, function(d) {return d.hours;})])
@@ -35,22 +34,20 @@ angular.module('voluntrApp')
          .attr("width", function(d) {return x(d.hours);})
          .attr("height", barHeight-1);
 
+      bar.append("text")
+         .attr("x", 5)
+         .attr("y", barHeight/2)
+         .attr("dy", ".35em")
+         .attr("fill", "#FFFFFF")
+         .text(function(d) { return d.name; });
+
+
      bar.append("text")
-        .attr("x", 5)
+        .attr("x", function(d) { return x(d.hours) + 5; })
         .attr("y", barHeight/2)
         .attr("dy", ".35em")
-        .attr("fill", "#FFFFFF")
-        .text(function(d) { return d.name; });
+        .text(function(d) { return d.hours + "hrs/person"; });
 
-
-    bar.append("text")
-       .attr("x", function(d) { return x(d.hours) + 5; })
-       .attr("y", barHeight/2)
-       .attr("dy", ".35em")
-       .text(function(d) { return d.hours + "hrs/person"; });
-
-
-      console.log(data);
     });
   }
   return {
