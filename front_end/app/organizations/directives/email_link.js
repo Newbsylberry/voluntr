17e4892d30044bbb6d1bb55b0bb1ae2da47d29f5
Peter B Smith <peterbutlersmith@gmail.com>
2016-04-13 11:05:04 -0400

    inital email volunteers modal

---
+++ front_end/app/organizations/directives/email_link.js
@@ -0,0 +1,15 @@
+angular.module('voluntrApp').directive("emailLink", function ($mdDialog) {
+  link: function link(scope, element, attrs) {
+    element.bind('click', function () {
+      var emailModal = $mdDialog.show({
+        controller: 'EmailVolunteersCtrl',
+        templateUrl: 'organizations/modals/email_volunteers.html',
+        parent: angular.element(document.body),
+        clickOutsideToClose: true
+      });
+    });
+  }
+  return {
+    link:  link
+  }
+});
---
+++ front_end/app/organizations/modals/email_volunteers.js
@@ -0,0 +1,13 @@
+/**
+ * Created by chrismccarthy on 3/9/15.
+ */
+angular.module('voluntrApp')
+.controller('EmailVolunteersCtrl', [
+'$scope',
+function($scope) {
+
+
+
+
+}
+]);
