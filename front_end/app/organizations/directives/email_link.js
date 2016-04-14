caf204423ad2b6d033af7f448bc76ad1aa9c184a
Peter B Smith <peterbutlersmith@gmail.com>
2016-04-14 12:03:33 -0400

    improving the email dialog

--- front_end/app/organizations/directives/email_link.js
+++ front_end/app/organizations/directives/email_link.js
@@ -1,14 +1,10 @@
 angular.module('voluntrApp').directive("emailLink", function ($mdDialog) {
   link: function link(scope, element, attrs) {
     element.bind('click', function () {
-      console.log(attrs);
-      console.log(attrs.role);
-      console.log(scope);
       var emailModal = $mdDialog.show({
         controller: 'EmailVolunteersCtrl',
         templateUrl: 'organizations/modals/email_volunteers.html',
         parent: angular.element(document.body),
-        clickOutsideToClose: true,
         locals: {
           volunteers: attrs.volunteers,
           role: attrs.role
