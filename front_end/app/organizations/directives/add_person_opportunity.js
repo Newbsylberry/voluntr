ea622ae2836877689dc9724c18308a622fd4d4a4
Peter B Smith <peterbutlersmith@gmail.com>
2016-04-15 18:29:45 -0400

    adding contact chips to add person opportunity directive

--- front_end/app/organizations/directives/add_person_opportunity.js
+++ front_end/app/organizations/directives/add_person_opportunity.js
@@ -9,11 +9,71 @@ angular.module('voluntrApp').directive("addPersonOpportunity", function () {
       instance: '='
     },
     restrict: 'E',
-    controller: function ($scope,$mdDialog,PersonOpportunity) {
-
+    controller: function ($scope,$mdDialog,$http,$stateParams,PersonOpportunity) {
+      var lastSearchTime;
+      var pendingSearch;
+      var cancelSearch = angular.noop;
+      var cachedQuery;
       $scope.openForm = function(opportunity_instances){
         $mdOpenMenu(opportunity_instances)
       };
+
+      $scope.anything = [{name: "Jane Doe"},{name: "John Doe"}];
+      $scope.contacts = [];
+      $scope.search = function (query) {
+        cachedQuery = cachedQuery || query;
+        return cachedQeury ? $scope.anything.filter(createFilterFor(cachedQuery)) : [];
+      }
+
+      $scope.asyncSearch = function(query) {
+        if (!pendingSearch || !debounceSearch()) {
+          cancelSearch();
+          return $http({
+            url: 'api/v1/organizations/' + $stateParams.organization_Id + '/people_search',
+            params: {query: query}
+          })
+          .then(function(response) {
+            console.log(response);
+            var filteredResponse = response.data.filter(function(object) {
+              for (var i = 0; i < $scope.contacts.length; i++) {
+                if($scope.contacts[i].id === object._source.person.id) {
+                  return false;
+                }
+              }
+              return true;
+            });
+            
+            return mappedResponse = filteredResponse.map(function(object) {
+              return {
+                id: object._source.person.id,
+                fullName: object._source.person.first_name + " " + object._source.person.last_name,
+                email: object._source.person.email
+              };
+            });
+
+            resetDebounce();
+          })
+        }
+      }
+
+
+      function resetDebounce() {
+        lastSearchTime = 0;
+        pendingSearch = null;
+        cancelSearch = angular.noop;
+      }
+
+      function debounceSearch() {
+        var now = new Date().getMilliseconds();
+        lastSearchTime = lastSearchTime || now;
+        return ((now - lastSearchTime) < 250);
+      }
+
+      function createFilterFor(query) {
+        return function filterFn(contact) {
+          return (contact.name.indexOf(query) != -1);
+        }
+      }
 
       $scope.person = {};
       $scope.person.person_id = {};
